f4671ec218d0e99ac29b05668f3fc2ad9dc841d2
daria.laptsevich <daria.laptsevich@team.wrike.com>
2019-01-09 00:55:31 +0300

    Group list for all types users

--- lib/src/components/models/user/regular_user.dart
+++ lib/src/components/models/user/regular_user.dart
@@ -1,3 +1,4 @@
+import 'package:angular_tour_of_heroes/src/components/models/group/group.dart';
 import 'package:angular_tour_of_heroes/src/components/models/user/user.dart';
 import 'package:angular_tour_of_heroes/src/enums/user_type.dart';
 
@@ -10,7 +11,7 @@ class RegularUser extends User{
     _isAdmin = admin;
   }
 
-  RegularUser(String id, DateTime regDate, String fullName, String email, bool isAdmin) : super(id, regDate, fullName, email, null){
+  RegularUser(String id, DateTime regDate, String fullName, String email, bool isAdmin, List<Group> groups) : super(id, regDate, fullName, email, groups){
     this.userType = UserType.REGULAR;
     this.isAdmin = isAdmin;
   }
@@ -18,6 +19,11 @@ class RegularUser extends User{
   @override
   Map toJson() => {'id':id, 'regDate':regDate.toString(), 'fullName':fullName, 'email':email, 'isAdmin':isAdmin, 'userType':userType.value};
 
-  factory RegularUser.fromJson(Map<String, dynamic> user) => RegularUser(user['id'], DateTime.parse(user['regDate']), user['fullName'], user['email'], user['isAdmin']);
-
+  factory RegularUser.fromJson(Map<String, dynamic> user) => RegularUser(
+      user['id'],
+      (user['regDate'] != null) ? DateTime.parse(user['regDate']) : null,
+      user['fullName'],
+      user['email'],
+      user['isAdmin'],
+      (user['userGroups'] != null) ? User.parseGroupList(user['userGroups']) : null);
 }
